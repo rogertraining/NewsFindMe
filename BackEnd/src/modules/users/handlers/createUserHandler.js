0a8538f1e40d2adf6e72e6265e2d81ca334ca44a
NatanaelSantz <santosnatanael553@gmail.com>
2022-03-17 16:50:02 -0300

    User controller implementado

--- BackEnd/src/modules/users/handlers/createUserHandler.js
+++ BackEnd/src/modules/users/handlers/createUserHandler.js
@@ -1,7 +1,18 @@
-import { Service } from "../services/service.js";
+export class UserController{
+   async handle(req, res){ 
+      const {name, password, email} = req.body
 
-export function createUserHandler(request, response) {
-  const { name, email, password } = request.body;
+      return res.send({ message: "Hello world" });
+   }
+}  
 
-  const createUserService = new Service
-}
+
+
+// const UsuarioController = require('../UsuarioController');
+// module.exports = (app) => {
+//    app.post('/usuario', UsuarioController.post);
+//    app.put('/usuario/:id', UsuarioController.put);
+//    app.delete('/usuario/:id', UsuarioController.delete);
+//    app.get('/usuarios', UsuarioController.get);
+//    app.get('/usuario/:id', UsuarioController.getById);
+// }
