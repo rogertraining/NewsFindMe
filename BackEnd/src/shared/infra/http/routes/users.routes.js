b6392cc4c86c232d8ac1b7a3c24dc8c76bc36403
matheusalmeida01 <matheus.almeida@think.br.com>
2022-03-29 15:22:17 -0300

    user delete route

--- BackEnd/src/shared/infra/http/routes/users.routes.js
+++ BackEnd/src/shared/infra/http/routes/users.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { createUserHandler } from "../../../../modules/users/handlers/createUserHandler.js"
 import { getUserByIdHandler } from "../../../../modules/users/handlers/getUserByIdHandler.js";
+import { deleteUserHandler } from "../../../../modules/users/handlers/deleteUserHandler.js";
 
 const usersRoutes = Router();
 
 usersRoutes.post("/", createUserHandler)
 usersRoutes.get("/:id", getUserByIdHandler)
+usersRoutes.delete("/:id", deleteUserHandler)
 
 export default usersRoutes;
