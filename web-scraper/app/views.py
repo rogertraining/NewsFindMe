5e1f72171a207726f1afd9a41b26f11c556c9e9c
guilherme-heroset <101722121+guilherme-heroset@users.noreply.github.com>
2022-03-31 15:42:04 -0300

    "scraper_function"

--- web-scraper/app/views.py
+++ web-scraper/app/views.py
@@ -9,40 +9,49 @@ app.config['JSON_AS_ASCII'] = False
 
 @app.route('/')
 def home():
-    num = 0
-    indice = 0
-    ultima_pagina = 2
-    dicionario = {}
-    
-    with open('preferencias.json', encoding='utf-8') as arquivo_preferencias:
-        preferencia = json.load(arquivo_preferencias)
-        preferencia = preferencia["Escolhas"]
+    cria_arquivo_vazio()
+    noticias = scraper('div', 'feed-post-body', 'img', 'bstn-fd-picture-image', 'a', 'feed-post-link gui-color-primary gui-color-hover', 'a', 'feed-post-link gui-color-primary gui-color-hover', 'span', 'feed-post-datetime')
 
-    with open('noticias.json', 'w') as arquivo_vazio:
+    return noticias
+
+def cria_arquivo_vazio():
+    with open(r'noticias.json', 'w') as arquivo_vazio:
             arquivo_vazio.write('')
 
+def retorna_preferencias(informacao):
+    with open(r'preferencias.json', encoding='utf-8') as arquivo_preferencias:
+        json_load = json.load(arquivo_preferencias)
+        preferencia = json_load[f'{informacao}']
+    return preferencia
+
+def scraper(tag, classe, tag_img, classe_img, tag_tittle, classe_tittle, tag_link, classe_link, tag_data, classe_data):
+    preferencia = retorna_preferencias('Escolhas')
+    indice = 0
+    ultima_pagina = 2
+    num = 0
+    dicionario = {}
     while indice < len(preferencia):
         for i in range(1, ultima_pagina):
             url_pag = f'https://g1.globo.com/{preferencia[indice]}/index/feed/pagina-{i}.ghtml'
             site = requests.get(url_pag)
             soup = BeautifulSoup(site.content, 'html.parser')
-            noticias = soup.find_all('div', class_='feed-post-body')
+            noticias_g1 = soup.find_all(f'{tag}', class_=f'{classe}')
 
-            with open('noticias.json', 'w', newline='', encoding='UTF-8') as arquivo:
-                for noticia in noticias:
+            with open(r'noticias.json', 'w', newline='', encoding='UTF-8') as arquivo:
+                for noticia in noticias_g1:
                     num += 1
-                    titulo = noticia.find('a', class_='feed-post-link gui-color-primary gui-color-hover').get_text().strip()
+                    titulo = noticia.find(f'{tag_tittle}', class_=f'{classe_tittle}').get_text().strip()
                     titulo = titulo.replace('\n', '')
-                    classe_noticia = noticia.find('a', class_='feed-post-link gui-color-primary gui-color-hover')
+                    classe_noticia = noticia.find(f'{tag_link}', class_=f'{classe_link}')
                     link_noticia = classe_noticia.get('href')
-                    classe_data = noticia.find('span', class_='feed-post-datetime').get_text().strip()
-                    data = classe_data.replace('-', '')
+                    classe_date = noticia.find(f'{tag_data}', class_=f'{classe_data}').get_text().strip()
+                    data = classe_date.replace('-', '')
 
                     try:
-                        link_imagem = noticia.find('img', class_='bstn-fd-picture-image').get('src')
+                        link_imagem = noticia.find(f'{tag_img}', class_=f'{classe_img}').get('src')
                     except:
                         link_imagem = '0'
-                
+                    
                     dicionario.update({f'Notícia {num}':{'Tittle': titulo, 'Notice': link_noticia, 'Image': link_imagem, 'Date': data}})
                 json.dump(dicionario, arquivo, indent=4, ensure_ascii=False)
 
