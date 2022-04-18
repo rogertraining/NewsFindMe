3c3d509694b061928e53d7232546279470c3950e
Isadora <isadora.silva@think.br.com>
2022-04-18 16:44:31 -0300

    capturando interesses do usuário

--- src/js/form-data.js
+++ src/js/form-data.js
@@ -1,27 +1,26 @@
-const btsLanding = document.querySelectorAll("#bts-landing button");
+const btsLanding = document.querySelectorAll(".buttons-landing button");
 const btCadastro = document.getElementById("bt-modal-cadastro");
-const btProximo = document.getElementById("bt-confirmar");
 const btFinalizaCadastro = document.getElementById("enviar-formulario-interesses");
 const btEntrar = document.getElementById("bt-modal-entrar");
 const btConfirmarEntrada = document.getElementById("bt-confirma-entrada");
 
-var resultado = "";
+var resultadoButton = "";
 
 for (var i = 0; i < btsLanding.length; i++) {
   btsLanding[i].addEventListener("click", function (e) {
-    resultado = this.innerHTML;
-    return resultado;
+    resultadoButton = this.innerHTML;
+    return resultadoButton;
   })
 }    
 
 const getForm = () => {
-  var form = resultado;
-
-  if(form == "Cadastre-se"){
-    form = document.getElementById("form-cadastro");
+  var form = resultadoButton;
+  
+  if(form == "Entrar"){
+    form = document.getElementById("form-entrar");
     return form;
   }else{
-    form = document.getElementById("form-entrar");
+    form = document.getElementById("form-cadastro");
     return form;
   }
 }
@@ -31,8 +30,20 @@ const getFormData = () => {
   return new FormData(form);
 }
 
+const getInterest = () =>{
+  let frmCheck = document.getElementById('form-interesses').elements;
+  let resultado = ""
+  for(i = 0; i < frmCheck.length; i++){
+    if(frmCheck[i].checked){
+      resultado = resultado + frmCheck[i].value + " ";
+      console.log(resultado)
+    }
+  }
+}
+
 const toJson = function(event) {
   const formData = getFormData();
+  const formCheck = getInterest();
   event.preventDefault();
 
   let object = {};
@@ -47,7 +58,7 @@ const toJson = function(event) {
     object[key].push(value);
   });
 
-  let json = JSON.stringify(Object.fromEntries(formData));
+  let json = JSON.stringify(Object.fromEntries(formData, formCheck));
   console.log(json);
 
   const options = {
@@ -61,5 +72,5 @@ const toJson = function(event) {
   fetch('http://localhost:8000/', options)
 };
 
-btProximo.addEventListener("click", toJson);
+btFinalizaCadastro.addEventListener("click", toJson);
 btConfirmarEntrada.addEventListener("click", toJson);
