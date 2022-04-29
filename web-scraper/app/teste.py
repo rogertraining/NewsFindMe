4e6602a422185c6f6fcdcf85e1716957754aa4ec
guilherme-2003 <guilherme.paulela2003@gmail.com>
2022-04-29 09:53:56 -0300

    Scraper final version

--- web-scraper/app/teste.py
+++
@@ -1,7 +0,0 @@
-dic = {"Notícia 1" : {"Tittle": 'Teste', 'Notice': 'Teste1'}, "Notícia 2": {'Tittle': 'Teste2', "Notice": "Teste3"}, "Notícia 3" : {"Tittle": 'Teste', 'Notice': 'Teste1'}}
-print(dic)
-dic2 = {}
-for key, value in dic.items():
-    if value not in dic2.values():
-        dic2[key] = value
-print(dic2)
